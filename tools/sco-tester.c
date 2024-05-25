ee8f97849c2ad101ace874df3975fc0042a778bd
Pauli Virtanen <pav@iki.fi>
2024-05-25 14:20:18 +0300

    tools/tester: test only COMPLETION timestamps
    Add support for SOF_TIMESTAMPING_TX_COMPLETION also in cases where
    errqueue.h is old and doesn't define it.
    
    Support timestamps of different types arriving out of order, as multiple
    SND may well arrive before COMPLETION.  Also allow TX timestamp arriving
    before bthost receives data, as that may well happen.
    
    Remove tests of SND and SCHED timestamps, as those won't be generated
    for now.

--- tools/sco-tester.c
+++ tools/sco-tester.c
@@ -290,7 +290,8 @@ static const struct sco_client_data connect_send_tx_timestamping = {
 	.send_data = data,
 	.so_timestamping = (SOF_TIMESTAMPING_SOFTWARE |
 					SOF_TIMESTAMPING_OPT_ID |
-					SOF_TIMESTAMPING_TX_SOFTWARE),
+					SOF_TIMESTAMPING_TX_SOFTWARE |
+					SOF_TIMESTAMPING_TX_COMPLETION),
 	.repeat_send = 2,
 };
 
@@ -637,10 +638,10 @@ static gboolean recv_errqueue(GIOChannel *io, GIOCondition cond,
 	err = tx_tstamp_recv(&data->tx_ts, sk, scodata->data_len);
 	if (err > 0)
 		return TRUE;
-	else if (!err && !data->step)
-		tester_test_passed();
-	else
+	else if (err)
 		tester_test_failed();
+	else if (!data->step)
+		tester_test_passed();
 
 	data->err_io_id = 0;
 	return FALSE;
@@ -654,7 +655,7 @@ static void sco_tx_timestamping(struct test_data *data, GIOChannel *io)
 	int err;
 	unsigned int count;
 
-	if (!(scodata->so_timestamping & SOF_TIMESTAMPING_TX_RECORD_MASK))
+	if (!(scodata->so_timestamping & TS_TX_RECORD_MASK))
 		return;
 
 	sk = g_io_channel_unix_get_fd(io);
