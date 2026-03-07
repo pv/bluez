c07a8f1d16a32c8ce6a1918a49df51cfad457535
Pauli Virtanen <pav@iki.fi>
2026-03-07 11:22:52 +0200

    test: functional: impose Python code formatting
    Check Python code formatting of the functional test suite.

---
+++ test/functional/test_tests.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8; mode: python; eval: (blacken-mode); -*-
+"""
+Tests for the test suite itself
+"""
+import sys
+import subprocess
+import warnings
+from pathlib import Path
+
+import pytest
+
+
+def test_formatting():
+    pytest.importorskip("black")
+
+    result = subprocess.run(
+        [sys.executable, "-mblack", "--check", "--diff", "-q", Path(__file__).parent],
+        stdout=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    if result.returncode != 0:
+        warnings.warn(f"Formatting incorrect:\n{result.stdout}")
